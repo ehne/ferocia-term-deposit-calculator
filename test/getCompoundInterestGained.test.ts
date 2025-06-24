04f6d9cf487693cf3d613021ecbe855e720a0a11
ehne <7551957+ehne@users.noreply.github.com>
2025-06-24 22:55:10 +1000

    test: add tests for getCompoundInterestGained when interest is paid annually

--- test/getCompoundInterestGained.test.ts
+++ test/getCompoundInterestGained.test.ts
@@ -52,4 +52,27 @@ describe("compound interest gained calculation works for 'normal' inputs", () =>
       ).toBe(expectedGain);
     },
   );
+
+  // interest paid frequency: annually
+  test.each([
+    // [starting balance, interest rate per-anum, months invested, expected gain]
+    [10000, 0.011, 12, 110],
+    [1000, 0.0245, 12 * 5, 129],
+    [54321, 0.011, 26, 1303],
+    [0, 0.011, 12, 0],
+    [54321, 0, 12, 0],
+  ])(
+    "interest paid: annually, principal: %i, interest rate: %f p.a., over %i months => $%i gained",
+    (principal, yearlyInterestRate, monthsInvested, expectedGain) => {
+      expect(
+        // converts yearly interest rate into monthly interest rate because the function expects it in that format.
+        getCompoundInterestGained(
+          principal,
+          yearlyInterestRate / 12.0,
+          monthsInvested,
+          InterestPayFrequency.ANNUALLY,
+        ),
+      ).toBe(expectedGain);
+    },
+  );
 });
