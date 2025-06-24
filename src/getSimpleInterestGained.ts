a65588ceee1d4fab0ea66f0f19dd8ff1c1b69a30
ehne <7551957+ehne@users.noreply.github.com>
2025-06-24 22:00:19 +1000

    feat: create very basic interface for getSimpleInterestGained

---
+++ src/getSimpleInterestGained.ts
@@ -0,0 +1,14 @@
+/**
+ * Returns the amount of interest gained when using simple interest.
+ *
+ * @param startingBalance the starting balance (aka. the "principal")
+ * @param monthlyInterestRate the interest rate per-month (instead of per-anum)
+ * @param monthsInvested how many months the money was invested for
+ */
+export const getSimpleInterestGained = (
+  startingBalance: number,
+  monthlyInterestRate: number,
+  monthsInvested: number,
+): number => {
+  return 0;
+};
