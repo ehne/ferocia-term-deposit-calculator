ab4a1250338684efc6440d3a3faa4d4e2dfb04d1
ehne <7551957+ehne@users.noreply.github.com>
2025-06-24 23:44:52 +1000

    feat: set up cli parsing of various inputs to the calculator

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,32 @@
-console.log("hello world!");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { InterestPayFrequency } from "../src/getCompoundInterestGained";
+
+const interestPaidOptions = [
+  ...Object.keys(InterestPayFrequency).filter((x) => isNaN(Number(x))),
+  "AT_MATURITY",
+];
+
+const args = yargs()
+  .scriptName("term-deposit-calculator")
+  .demandOption([
+    "investment-term",
+    "interest-rate",
+    "starting-balance",
+    "interest-paid",
+  ])
+  .choices("interest-paid", interestPaidOptions)
+  .number(["investment-term", "interest-rate", "starting-balance"])
+  .describe(
+    "investment-term",
+    "how many months you want to invest your money for",
+  )
+  .describe(
+    "interest-rate",
+    "the per-anum interest rate (as a number from 0 to 100)",
+  )
+  .describe("starting-balance", "the amount of money you want to invest")
+  .help()
+  .parseSync(hideBin(process.argv));
+
+console.log(args);
